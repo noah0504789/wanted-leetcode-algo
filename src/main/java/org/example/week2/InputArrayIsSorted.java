d7cd21a679ea8966e227fa89f902e1ea30a21ee2
noah0504789 <noah0504@naver.com>
2023-08-27 21:48:22 +0900

    W1: 167. Two Sum II - Input Array Is Sorted

---
+++ src/main/java/org/example/week2/InputArrayIsSorted.java
@@ -0,0 +1,28 @@
+package org.example.week2;
+
+public class InputArrayIsSorted {
+    public static void main(String[] args) {
+        int[] numbers = new int[]{2,7,11,15};
+        int target = 9;
+
+        myTwoSum(numbers, target);
+    }
+
+    public static int[] myTwoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            if (numbers[left] + numbers[right] == target) {
+                break;
+
+            } else if (numbers[left] + numbers[right] > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+
+        return new int[]{left+1, right+1};
+    }
+}
