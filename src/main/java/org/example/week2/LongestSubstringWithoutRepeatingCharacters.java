f813cde029aefd219bed18d4fada22f7ebad5ef5
noah0504789 <noah0504@naver.com>
2023-08-28 16:38:26 +0900

    W1: 3. Longest Substring Without Repeating Characters

---
+++ src/main/java/org/example/week2/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,60 @@
+package org.example.week2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// 출처 : https://leetcode.com/problems/longest-substring-without-repeating-characters/?envType=study-plan-v2&envId=top-interview-150
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        int i = myLengthOfLongestSubstring(s);
+        solutionLengthOfLongestSubstring(s);
+    }
+
+    private static int solutionLengthOfLongestSubstring(String s) {
+        // 출처 : https://www.youtube.com/watch?v=3IETreEybaA&ab_channel=NickWhite
+        int i = 0;
+        int j = 0;
+        int max = 0;
+
+        Set<Character> hashSet = new HashSet<>();
+
+        while (j < s.length()) {
+            if (!hashSet.contains(s.charAt(j))) {
+                hashSet.add(s.charAt(j));
+                j++;
+                max = Math.max(max, hashSet.size());
+            } else {
+                hashSet.remove(s.charAt(i));
+                i++;
+            }
+        }
+
+        return max;
+    }
+
+    public static int myLengthOfLongestSubstring(String s) {
+        int ans=0;
+        int left = 0;
+
+        if (s.length() == 0) {
+            return ans;
+        }
+
+        for (int right = 1; right < s.length(); right++) {
+            for (int i = left; i < right; i++) {
+                if (s.charAt(i) == s.charAt(right)) {
+                    left = i+1;
+                    break;
+                }
+            }
+
+            if (ans < right - left) {
+                ans = right - left;
+            }
+        }
+
+        return ans+1;
+    }
+}
